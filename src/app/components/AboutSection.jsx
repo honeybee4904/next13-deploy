827bd214c2ac656ae23b5ba644871114de5a606c
honeybee4904 <honeybee4904@gmail.com>
2025-02-24 14:34:08 +0800

    first commit

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -36,7 +36,6 @@ const TAB_DATA = [
     content: (
       <ul className="list-disc pl-2">
         <li>Networking Basics</li>
-        
       </ul>
     ),
   },
@@ -55,11 +54,12 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/about-image.png" width={500} height={500} />
+        {/* Updated Image with alt attribute */}
+        <Image src="/images/about-image.png" width={500} height={500} alt="A picture of Jebecson Bantilan" />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-          Hello, my name is Jebecson Bantilan, and I am 22 years old, currently pursuing a Bachelor of Science in Information Technology at Zamboanga Peninsula Polytechnic State University. I live in Cabatangan, Zamboanga City. I have a strong passion for developing innovative systems, particularly in the areas of web-based applications and hardware-software integration for IoT projects. My skills allow me to create seamless solutions that bridge the gap between hardware and software, enhancing user experiences and advancing technology in meaningful ways.
+            Hello, my name is Jebecson Bantilan, and I am 22 years old, currently pursuing a Bachelor of Science in Information Technology at Zamboanga Peninsula Polytechnic State University. I live in Cabatangan, Zamboanga City. I have a strong passion for developing innovative systems, particularly in the areas of web-based applications and hardware-software integration for IoT projects. My skills allow me to create seamless solutions that bridge the gap between hardware and software, enhancing user experiences and advancing technology in meaningful ways.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
